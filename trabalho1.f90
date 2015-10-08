53c453b7f27dbcdd065e10bd116c285c317ea2d3
re_junges <renatajunges@hotmail.com>
2015-10-08 14:45:24 -0300

    Teste pro yan

--- trabalho1.f90
+++ trabalho1.f90
@@ -1,13 +1,17 @@
 PROGRAM trabalho1 !Inicio do trabalho de clp em fortran
 	IMPLICIT NONE !Todas as variáveis terão que ter seus tipos definidos IMPLICITAMENTE
 	!Declaração de variáveis
-	CHARACTER (LEN = 10 ) :: first !Primeira linha
-	CHARACTER (LEN = 1000) :: second !Segunda linha
+	CHARACTER *10 first !Primeira linha-> equivalente a first[10]
+	CHARACTER *1000 second !Segunda linha-> equivalente a second[1000]
 	!Abertura de arquivo
 	OPEN(UNIT=50,FILE="entrada.txt") !Leitura de arquivo, demais atributos não são obrigados (status, action)
 	!leitura do arquivo
 	READ(50, *) first  !Le a primeira linha do arquivo
 	READ(50,*) second !Le a segunda liha do arquivo
+	print *, first
+	print *, second
 	CLOSE(50) !Fecha o arquivo
+	!Chama a subrotina, e passa os 2 parametros ( primeira linha e segunda linha )
+	!CALL numVezesEstaContida(first, second) 
 	
 END PROGRAM trabalho1
