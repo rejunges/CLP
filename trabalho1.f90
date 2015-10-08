956ed1bdc3b124ed4d899b4cbc0386df5d7ca43c
re_junges <renatajunges@hotmail.com>
2015-10-08 10:52:59 -0300

    Retirei o print que tinha e adicionei comentários

--- trabalho1.f90
+++ trabalho1.f90
@@ -1,11 +1,13 @@
 PROGRAM trabalho1 !Inicio do trabalho de clp em fortran
 	IMPLICIT NONE !Todas as variáveis terão que ter seus tipos definidos IMPLICITAMENTE
-	
+	!Declaração de variáveis
 	CHARACTER (LEN = 10 ) :: first !Primeira linha
 	CHARACTER (LEN = 1000) :: second !Segunda linha
+	!Abertura de arquivo
 	OPEN(UNIT=50,FILE="entrada.txt") !Leitura de arquivo, demais atributos não são obrigados (status, action)
+	!leitura do arquivo
 	READ(50, *) first  !Le a primeira linha do arquivo
 	READ(50,*) second !Le a segunda liha do arquivo
-	print *, first
 	CLOSE(50) !Fecha o arquivo
+	
 END PROGRAM trabalho1
