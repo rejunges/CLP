c32da809e7cb8239b05f18410cb9017ac41d4f9b
re_junges <renatajunges@hotmail.com>
2015-10-08 14:31:10 -0300

    Quebrei o programa, criei uma função para verificar o numero de vezes para facilitar o código quando precisarmos misturar ele com fortran. Para misturar códigos ficamos chamando funções do outro código, eu disse FUNÇÕES! POr isso não é mais sequencial. Testei, funciona.

--- trabalho1.c
+++ trabalho1.c
@@ -2,10 +2,11 @@
 #include <string.h>
 //o número de vezes que a primeira string é encontrada na segunda
 
+int numeroVezes(char *pont1,char *pont2);	
 int main(void) {
 	FILE *arquivo; // estrutura para usar arquivo
-	int cont=0;
-	char linha1[11], linha2[1001], *pont1, *pont2, *base1, *base2; // cria uma 'string' para a linha 1 e a linha 2
+	int cont;
+	char linha1[11], linha2[1001], *pont1, *pont2; // cria uma 'string' para a linha 1 e a linha 2
 	arquivo = fopen("entrada.txt", "r");// abre o programa para read-leitura
 	if (arquivo == NULL){ // se o arquivo é NULL significa que ele não existe
 		printf("Erro ao abrir o arquivo\n");
@@ -14,28 +15,27 @@ int main(void) {
 	pont1=fgets(linha1, 12, arquivo);  // esse fgets lê a primeira linha
 	pont2=fgets(linha2, 1002, arquivo);  // esse fgets lê a segunda linha
 	fclose(arquivo); //fecha o programa
-	//printf("Linha 1: %s\n", linha1);
-	//printf("Linha 2: %s\n", linha2);
+	cont=numeroVezes(pont1, pont2); //Chama a função que verifica o número de vezes 
+	printf ("%d", cont);
+	return 0;
+
+}
+int numeroVezes(char *pont1,char *pont2){
+	char *base1, *base2;
+	int cont=0;
 	base1 = pont1;
 	base2 = pont2;
 	while(*pont2 != '\n'){
-		//printf("Ta pegando no ponteiro 1: %c\n", *pont1);
-		//printf("Ta pegando no ponteiro 2: %c\n", *pont2);
 		while(*pont1 == *pont2){
-			//printf("Ta pegando no ponteiro 1: %c\n", *pont1);
-			//printf("Ta pegando no ponteiro 2: %c\n", *pont2);
 			pont1++;
 			pont2++;
 			if(*pont1 == '\n'){
 				cont++; //achou a subpalavra
-				//printf("ACHOOOOU\n");
 			}
 		}
 		base2++;
 		pont1 = base1;
 		pont2 = base2;
 	}
-	printf ("%d", cont);
-	return 0;
-
+	return cont;
 }
