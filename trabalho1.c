578f8774d639875f80b1f09a97221c1cf3d0aa46
Matheus Neiverth <matheus.ropke.neiverth@gmail.com>
2015-10-08 21:48:40 -0300

    added external structs in both side

--- trabalho1.c
+++ trabalho1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 //o número de vezes que a primeira string é encontrada na segunda
+extern int substring(char* s, int* length, char* subs, int* lengths); //estrutura pra pegar a string em java
 
 int numeroVezes(char *pont1,char *pont2);	
 int main(void) {
