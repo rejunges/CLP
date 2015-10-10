b949763cd5751ffb159a99d349702269683f16d4
Matheus Neiverth <matheus.ropke.neiverth@gmail.com>
2015-10-09 21:22:44 -0300

    a

---
+++ makefile
@@ -0,0 +1,5 @@
+to_compile_and_link:
+	gcc -c main.c		#gera .o
+	gcc -o main.exe main.o	#linka .o's
+to_run:
+	./main			#run the output
--- trabalho1.c
+++ trabalho1.c
@@ -2,13 +2,13 @@
 #include <string.h>
 //o número de vezes que a primeira string é encontrada na segunda
 extern int substring(char* s, int* length, char* subs, int* lengths); //estrutura pra pegar a string em java
-
-int numeroVezes(char *pont1,char *pont2);	
+int numeroVezes(char *pont1,char *pont2);
+	
 int main(void) {
 	FILE *arquivo; // estrutura para usar arquivo
 	int cont;
 	char linha1[11], linha2[1001], *pont1, *pont2; // cria uma 'string' para a linha 1 e a linha 2
-	arquivo = fopen("entrada.txt", "r");// abre o programa para read-leitura
+	arquivo = fopen("entrada.txt", "r+");// abre o programa para read-leitura
 	if (arquivo == NULL){ // se o arquivo é NULL significa que ele não existe
 		printf("Erro ao abrir o arquivo\n");
 		return 0;//fecha o programa caso o arquivo seja NULO
@@ -16,11 +16,12 @@ int main(void) {
 	pont1=fgets(linha1, 12, arquivo);  // esse fgets lê a primeira linha
 	pont2=fgets(linha2, 1002, arquivo);  // esse fgets lê a segunda linha
 	fclose(arquivo); //fecha o programa
-	cont=numeroVezes(pont1, pont2); //Chama a função que verifica o número de vezes 
+	cont = numeroVezes(pont1, pont2); //Chama a função que verifica o número de vezes 
 	printf ("%d", cont);
 	return 0;
 
 }
+
 int numeroVezes(char *pont1,char *pont2){
 	char *base1, *base2;
 	int cont=0;
