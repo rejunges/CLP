f91e7b8f5a113583c08ce8bad997e570c5188618
Renata Junges <rzjunges@inf.ufpel.edu.br>
2015-10-11 19:15:27 -0300

    Pastas separadas para cada arquivo ( por causa do makefile )

--- C /biblioteca.h
+++ C /biblioteca.h
@@ -1,5 +1,5 @@
 #include <string.h>
 #include <stdio.h>
-#include "biblioteca.c"
+
 int numeroVezesC(char *linha1,char *linha2, int tam1, int tam2)
 
--- C /trabalho1.c
+++ C /trabalho1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 //o número de vezes que a primeira string é encontrada na segunda
-int numeroVezesC(char *linha1,char *linha2, int tam1, int tam2);
 	
 int main(void) {
 	FILE *arquivo; // estrutura para usar arquivo
@@ -35,22 +34,3 @@ int main(void) {
 
 }
 
-int numeroVezesC(char *linha1,char *linha2, int tam1, int tam2){
-	int cont=0,i=0,j=0, n=0;
-	char aux[tam1]; // string auxiliar para fazer comparações
-	
-	while (j != tam2){
-		n++;
-		while (i!=tam1){
-			aux[i]=linha2[j];
-			j++;
-			i++;
-		}
-		j=n; //para garantir que encontrará substrings que serão sobrescritas
-		i=0;
-		if (strcmp(linha1, aux)==0){ //strings iguais, achou 1 substring
-			cont++; //achou a subpalavra
-		}
-	}
-	return cont;
-}
